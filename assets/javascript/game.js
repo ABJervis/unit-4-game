9aad3724f23dc8ffe611c83b7b83957b4edc3397
Blues <40874591+ABJervis@users.noreply.github.com>
2018-09-17 22:27:50 -0400

    working on the logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+
+//assign random number from start..The random number shown at the start of the game should be between 19 - 120.
+
+var computerNumber = Math.floor(Math.random() * (120 - 19) + 19);
+console.log(computerNumber)
+
+//define variables
+
+var jewelValues = [];
+var total = 0;
+var wins = 0;
+var losses = 0;
+
+
+//assign randon number to each jewel..Each crystal should have a random hidden value between 1 - 12.
+
+
+function jewelNumbers(){
+for (var i = 0; i < 4; i++) { 
+    var fourRandom = Math.floor(Math.random()* 12) + 1;
+    jewelValues.push(fourRandom);
+}} 
+console.log(jewelValues)
+
+jewelNumbers();
+
+
+//link onclick to jewels
+
+
+//execute addition
+
+//conditionals - compare computer number to user number
+
+//tally for score, wins, losses
+
+//reset game
+
