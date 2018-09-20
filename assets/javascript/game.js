b5167ecd45780ead0121b84f2ee67ad6f6186235
Blues <40874591+ABJervis@users.noreply.github.com>
2018-09-19 22:04:57 -0400

    jewel click works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,9 +11,7 @@ var total = 0;
 var wins = 0;
 var losses = 0;
 
-
 //assign randon number to each jewel..Each crystal should have a random hidden value between 1 - 12.
-
 
 function jewelNumbers(){
 for (var i = 0; i < 4; i++) { 
@@ -26,6 +24,34 @@ jewelNumbers();
 
 
 //link onclick to jewels
+
+$(document).ready(function(){
+    $("#one").click(function(){
+        total = total + jewelValues[0]
+        $("#addedScore").text(total);
+    });
+});
+
+$(document).ready(function(){
+    $("#two").click(function(){
+        total = total + jewelValues[1]
+        $("#addedScore").text(total);
+    });
+});    
+
+$(document).ready(function(){
+    $("#three").click(function(){
+        total = total + jewelValues[2]
+        $("#addedScore").text(total);
+    });
+});
+
+$(document).ready(function(){
+    $("#four").click(function(){
+        total = total + jewelValues[3]
+        $("#addedScore").text(total);
+    });
+});           
 
 
 //execute addition
